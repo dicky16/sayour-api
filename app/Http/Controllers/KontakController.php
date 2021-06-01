296b7bbe809ba77c1210d11102ebd72884913066
Dikky Setiawan <setyawandicky88@gmail.com>
2021-06-01 23:01:03 +0700

    api

--- app/Http/Controllers/KontakController.php
+++ app/Http/Controllers/KontakController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KontakController extends Controller
 {
-    //
+    public function index()
+    {
+        try {
+            $data = DB::table('kontak')->orderBy('id', 'desc')->get();
+            return response()->json([
+                'status' => true,
+                'data' => $data
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'msg' => $th->getMessage()
+            ]);
+        }
+    }
 }
